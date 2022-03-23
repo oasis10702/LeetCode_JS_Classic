d988e62a500acd8c5221323bdc25d438c4faef80
oasis10702 <oasis10702@gmail.com>
2022-03-23 21:18:02 +0900

    Implement lc5'

--- exercises/LC5_longestPalindrome/index.js
+++ exercises/LC5_longestPalindrome/index.js
@@ -5,6 +5,20 @@
 // longestPalindrome("a") --> "a"
 
 function longestPalindrome(s) {
+  let startIndex = 0;
+  let maxLength = 1;
+
+  function expandAroundMiddle(left, right) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      const currentPalLength = right - left + 1;
+      if (currentPalLength > maxLength) {
+        maxLength = currentPalLength;
+        startIndex = left;
+      }
+      left -= 1;
+      right += 1;
+    }
+  }
   // @TODO
 }
 
