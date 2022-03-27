1c400f75c2daca675aebc386361c343513394435
oasis10702 <oasis10702@gmail.com>
2022-03-27 20:50:44 +0900

    Start lc3

--- exercises/LC3_lengthOfLongestSubstring/index.js
+++ exercises/LC3_lengthOfLongestSubstring/index.js
@@ -4,6 +4,10 @@
 // lengthOfLongestSubstring("abcabcbb") --> 3 since length of "abc"
 // lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
 
-function lengthOfLongestSubstring(s) {}
+function lengthOfLongestSubstring(s) {
+  let windowCharsMap = {};
+  let windowStart = 0;
+  let maxLength = 0;
+}
 
 module.exports = lengthOfLongestSubstring;
