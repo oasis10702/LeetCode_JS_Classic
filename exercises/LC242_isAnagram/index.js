bb8014d5de35bf4ba5eb422113631c73b2970411
oasis10702 <oasis10702@gmail.com>
2022-04-10 15:12:36 +0900

    Finish lc242

--- exercises/LC242_isAnagram/index.js
+++ exercises/LC242_isAnagram/index.js
@@ -5,7 +5,22 @@ function isAnagram(s, t) {
 
   const sCharCounts = {}; // {H: 1, E: 1, L: 2, O: 1}
 
-  for (let i = 0; i < s.length; i++) {}
+  for (let i = 0; i < s.length; i++) {
+    const sChar = s[i];
+    sCharCounts[sChar] = sCharCounts[sChar] + 1 || 1;
+  }
+
+  for (let i = 0; i < t.length; i++) {
+    const tChar = t[i];
+
+    if (!sCharCounts[tChar]) {
+      return false;
+    } else {
+      sCharCounts[tChar]--;
+    }
+  }
+
+  return true;
 }
 
 module.exports = isAnagram;
