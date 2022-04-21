f14d3f858c3797174829987c060fd9d529cfe19a
oasis10702 <oasis10702@gmail.com>
2022-04-21 22:39:18 +0900

    Fix

--- exercises/LC20_isValid/index.js
+++ exercises/LC20_isValid/index.js
@@ -2,7 +2,9 @@ const isValid = (s) => {
   let stack = [];
   let pairsHashMap = { "(": ")", "{": "}", "[": "]" };
 
-  for (let i = 0; i < s.length; i++) {}
+  for (let i = 0; i < s.length; i++) {
+    // @TODO
+  }
 };
 
 module.exports = isValid;
