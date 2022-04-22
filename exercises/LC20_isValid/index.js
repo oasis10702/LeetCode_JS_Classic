a3c2c87e492c88470601050464f9773c8cc49fc6
oasis10702 <oasis10702@gmail.com>
2022-04-22 22:53:28 +0900

    Finish lc20

--- exercises/LC20_isValid/index.js
+++ exercises/LC20_isValid/index.js
@@ -3,8 +3,16 @@ const isValid = (s) => {
   let pairsHashMap = { "(": ")", "{": "}", "[": "]" };
 
   for (let i = 0; i < s.length; i++) {
-    // @TODO
+    let char = s[i];
+
+    if (pairsHashMap[char]) {
+      stack.push(char);
+    } else if (pairsHashMap[stack.pop()] !== char) {
+      return false;
+    }
   }
+
+  return stack.length === 0;
 };
 
 module.exports = isValid;
