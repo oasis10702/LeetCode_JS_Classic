64db5cb1eb42cdb31268e7946bcbe255cc78d14f
oasis10702 <oasis10702@gmail.com>
2022-04-28 22:23:15 +0900

    Fix

--- exercises/LC70_climbStairs/index.js
+++ exercises/LC70_climbStairs/index.js
@@ -3,7 +3,9 @@ const climbStairs = (n) => {
 
   let ways = [0, 1, 2, 3];
 
-  for (let i = 4; i <= n; i++) {}
+  for (let i = 4; i <= n; i++) {
+    ways.push(way[i - 1] + ways[i - 2]);
+  }
 };
 
 module.exports = climbStairs;
