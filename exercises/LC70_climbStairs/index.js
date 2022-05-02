07b43c4f30f89eb0fb9d63b096286aedf4b15028
oasis10702 <oasis10702@gmail.com>
2022-05-02 21:39:35 +0900

    Fix

--- exercises/LC70_climbStairs/index.js
+++ exercises/LC70_climbStairs/index.js
@@ -4,7 +4,7 @@ const climbStairs = (n) => {
   let ways = [0, 1, 2, 3];
 
   for (let i = 4; i <= n; i++) {
-    ways.push(way[i - 1] + ways[i - 2]);
+    ways.push(ways[i - 1] + ways[i - 2]);
   }
 
   return ways.pop();
