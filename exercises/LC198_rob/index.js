abf29e5e4054472eeaaa6811b0e77ee714ddd426
oasis10702 <oasis10702@gmail.com>
2022-05-08 22:14:26 +0900

    Add case 2 house

--- exercises/LC198_rob/index.js
+++ exercises/LC198_rob/index.js
@@ -1,6 +1,7 @@
 function rob(nums) {
   if (nums.length === 0) return 0;
   if (nums.length === 1) return nums[0];
+  if (nums.length === 2) return Math.max(nums[0], nums[1]);
 }
 
 module.exports = rob;
