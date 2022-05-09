cd93c45c487d678e7e2a773b772a7f4f4f2307c0
oasis10702 <oasis10702@gmail.com>
2022-05-09 23:49:31 +0900

    Fix

--- exercises/LC198_rob/index.js
+++ exercises/LC198_rob/index.js
@@ -2,6 +2,8 @@ function rob(nums) {
   if (nums.length === 0) return 0;
   if (nums.length === 1) return nums[0];
   if (nums.length === 2) return Math.max(nums[0], nums[1]);
+
+  // @TODO
 }
 
 module.exports = rob;
