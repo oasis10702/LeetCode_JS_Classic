d5efdeef08a24e686ec935ef5fd2353bbd56939d
oasis10702 <oasis10702@gmail.com>
2022-05-19 22:29:20 +0900

    Add loop

--- exercises/LC55_canJump/index.js
+++ exercises/LC55_canJump/index.js
@@ -1,7 +1,10 @@
 function canJump(nums) {
   let dpPositions = new Array(nums.length).fill(false);
-
   dpPositions[0] = true;
+
+  for (let j = 1; j < nums.length; j++) {
+    for (let i = 0; i < j; i++) {}
+  }
 }
 
 module.exports = canJump;
