8e45ee60e611df2b94b640b18f2c6df7a5bbc49f
oasis10702 <oasis10702@gmail.com>
2022-05-31 22:41:15 +0900

    Add loop

--- exercises/LC300_lengthOfLIS/index.js
+++ exercises/LC300_lengthOfLIS/index.js
@@ -3,6 +3,12 @@ function lengthOfLIS(nums) {
 
   let dpSubsequence = new Array(nums.length).fill(1);
   let maxSoFar = 1;
+
+  for (let j = 1; i < nums.length; j++) {
+    for (let i = 0; i < j; i++) {
+      // @TODO
+    }
+  }
 }
 
 module.exports = lengthOfLIS;
