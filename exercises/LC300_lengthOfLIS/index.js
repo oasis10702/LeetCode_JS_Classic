78a3ad1c3d332ecd49b1afce74c48df93f80d415
oasis10702 <oasis10702@gmail.com>
2022-06-12 21:09:04 +0900

    Fix math

--- exercises/LC300_lengthOfLIS/index.js
+++ exercises/LC300_lengthOfLIS/index.js
@@ -7,7 +7,7 @@ function lengthOfLIS(nums) {
   for (let j = 1; j < nums.length; j++) {
     for (let i = 0; i < j; i++) {
       if (nums[j] > nums[i]) {
-        dpSubsequence[j] = Matn.max(dpSubsequence[i] + 1, dpSubsequence[j]);
+        dpSubsequence[j] = Math.max(dpSubsequence[i] + 1, dpSubsequence[j]);
       }
     }
     maxSoFar = Math.max(maxSoFar, dpSubsequence[j]);
