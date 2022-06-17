d595664eefbc68a53f9bc77d0af57bd12cac45f9
oasis10702 <oasis10702@gmail.com>
2022-06-17 23:18:16 +0900

    Add loop

--- exercises/LC322_coinChange/index.js
+++ exercises/LC322_coinChange/index.js
@@ -2,6 +2,8 @@ function coinChange(coins, amount) {
   let dpMinCoins = new Array(amount + 1).fill(Infinity);
 
   dpMinCoins[0] = 0;
+
+  for (let i = 1; i < dpMinCoins.length; i++) {}
 }
 
 module.exports = coinChange;
