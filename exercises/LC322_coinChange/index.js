bc2b3dd236da05734a1a4e312263d8d9d43ad2e7
oasis10702 <oasis10702@gmail.com>
2022-06-20 23:17:36 +0900

    Get coin j

--- exercises/LC322_coinChange/index.js
+++ exercises/LC322_coinChange/index.js
@@ -4,7 +4,9 @@ function coinChange(coins, amount) {
   dpMinCoins[0] = 0;
 
   for (let i = 1; i < dpMinCoins.length; i++) {
-    for (let j = 0; j < coins.length; j++) {}
+    for (let j = 0; j < coins.length; j++) {
+      const coinValue = coins[j];
+    }
   }
 }
 
