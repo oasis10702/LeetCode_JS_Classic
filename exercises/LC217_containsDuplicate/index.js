4dcb133da701de450d26c7c1dd3ad353406ed042
oasis10702 <oasis10702@gmail.com>
2022-07-25 23:32:34 +0900

    Add num

--- exercises/LC217_containsDuplicate/index.js
+++ exercises/LC217_containsDuplicate/index.js
@@ -1,7 +1,9 @@
 const containsDuplicate = (nums) => {
   const visitedNums = {}; // { 7: true, 2: true }
 
-  for (let i = 0; i < nums.length; i++) {}
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+  }
 };
 
 module.exports = containsDuplicate;
