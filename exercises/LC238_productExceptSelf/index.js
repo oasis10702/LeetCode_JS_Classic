eeebf375216b435a6f802476beb302297be920a6
oasis10702 <oasis10702@gmail.com>
2022-08-10 23:44:28 +0900

    Add todo

--- exercises/LC238_productExceptSelf/index.js
+++ exercises/LC238_productExceptSelf/index.js
@@ -3,7 +3,9 @@ const productExceptSelf = (nums) => {
   let product = 1;
 
   // Multiply from the left
-  for (let i = 0; i < nums.length; i++) {}
+  for (let i = 0; i < nums.length; i++) {
+    // @TODO
+  }
 };
 
 module.exports = productExceptSelf;
