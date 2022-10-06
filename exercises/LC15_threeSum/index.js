b8e78b910e1c7df1880f6ebd353b903b7b0d1544
oasis10702 <oasis10702@gmail.com>
2022-10-06 22:11:12 +0900

    Add target

--- exercises/LC15_threeSum/index.js
+++ exercises/LC15_threeSum/index.js
@@ -2,7 +2,9 @@ const threeSum = (nums) => {
   nums.sort((a, b) => a - b);
   let res = [];
 
-  for (let i = 0; i < nums.length; i++) {}
+  for (let i = 0; i < nums.length; i++) {
+    const target = 0 - nums[i];
+  }
 };
 
 module.exports = threeSum;
