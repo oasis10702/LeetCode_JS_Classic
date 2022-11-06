9b67f74e4b04d46f849c0fa8bb9050ee7b5bef75
oasis10702 <oasis10702@gmail.com>
2022-11-06 21:15:43 +0900

    Add left

--- exercises/LC15_threeSum/index.js
+++ exercises/LC15_threeSum/index.js
@@ -15,7 +15,9 @@ const threeSum = (nums) => {
       if (nums[left] + nums[right] === target) {
         res.push([nums[i], nums[left], nums[right]]);
         left++;
-        while (nums[left] === nums[left - 1]) {}
+        while (nums[left] === nums[left - 1]) {
+          left++;
+        }
       } else if (nums[left] + nums[right] < target) {
         left++;
       } else {
