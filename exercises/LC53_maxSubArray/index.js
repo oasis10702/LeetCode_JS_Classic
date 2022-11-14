9fec04724a3bba1478cf6b949fd36189a2ee72d3
oasis10702 <oasis10702@gmail.com>
2022-11-14 23:16:31 +0900

    dp i

--- exercises/LC53_maxSubArray/index.js
+++ exercises/LC53_maxSubArray/index.js
@@ -2,7 +2,9 @@ function maxSubArray(nums) {
   let dp = [nums[0]];
   let max = nums[0];
 
-  for (let i = 1; i < nums.length; i++) {}
+  for (let i = 1; i < nums.length; i++) {
+    console.log(dp[i]);
+  }
 }
 
 module.exports = maxSubArray;
