2a3847de97e12a0449b5fc1f83c0de27d4cf0bce
oasis10702 <oasis10702@gmail.com>
2022-12-04 23:25:33 +0900

    Update nums

--- exercises/LC152_maxProduct/index.js
+++ exercises/LC152_maxProduct/index.js
@@ -1,7 +1,9 @@
 function maxProduct(nums) {
   let max = nums[0];
 
-  for (let i = 1; i < nums.length; i++) {}
+  for (let i = 1; i < nums.length; i++) {
+    num[i] = Math.max();
+  }
 }
 
 module.exports = maxProduct;
