cf5e08a417a5090dca710d6535f790320f08ad40
oasis10702 <oasis10702@gmail.com>
2022-12-11 22:54:35 +0900

    Fix

--- exercises/LC152_maxProduct/index.js
+++ exercises/LC152_maxProduct/index.js
@@ -1,12 +1,10 @@
 function maxProduct(nums) {
-  let max = nums[0];
-
-  for (let i = 1; i < nums.length; i++) {
-    num[i] = Math.max(nums[i], nums[i] * nums[i - 1]);
-    max = Math.max(max, nums[i]);
-  }
-
-  return max;
+  // let max = nums[0];
+  // for (let i = 1; i < nums.length; i++) {
+  //   num[i] = Math.max(nums[i], nums[i] * nums[i - 1]);
+  //   max = Math.max(max, nums[i]);
+  // }
+  // return max;
 }
 
 module.exports = maxProduct;
