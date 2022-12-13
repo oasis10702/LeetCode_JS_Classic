d0e0c3384546e5eac6c4276a916279bf70cfd006
oasis10702 <oasis10702@gmail.com>
2022-12-13 23:35:46 +0900

    Delete

--- exercises/LC152_maxProduct/index.js
+++ exercises/LC152_maxProduct/index.js
@@ -1,10 +1,3 @@
-function maxProduct(nums) {
-  // let max = nums[0];
-  // for (let i = 1; i < nums.length; i++) {
-  //   num[i] = Math.max(nums[i], nums[i] * nums[i - 1]);
-  //   max = Math.max(max, nums[i]);
-  // }
-  // return max;
-}
+function maxProduct(nums) {}
 
 module.exports = maxProduct;
