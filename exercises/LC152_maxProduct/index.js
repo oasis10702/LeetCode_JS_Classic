5e2048c1eb770f6f9b7f183daea428786c792122
oasis10702 <oasis10702@gmail.com>
2022-12-17 23:18:34 +0900

    Add loop

--- exercises/LC152_maxProduct/index.js
+++ exercises/LC152_maxProduct/index.js
@@ -2,6 +2,8 @@ function maxProduct(nums) {
   let maxTillIndex = [nums[0]];
   let minTillIndex = [nums[0]];
   let max = nums[0];
+
+  for (let i = i; i < nums.length; i++) {}
 }
 
 module.exports = maxProduct;
