88df845e1ca3520ddcb392d4cee0b7e8478bdf82
oasis10702 <oasis10702@gmail.com>
2022-12-19 23:37:02 +0900

    Count num

--- exercises/LC152_maxProduct/index.js
+++ exercises/LC152_maxProduct/index.js
@@ -3,7 +3,9 @@ function maxProduct(nums) {
   let minTillIndex = [nums[0]];
   let max = nums[0];
 
-  for (let i = i; i < nums.length; i++) {}
+  for (let i = i; i < nums.length; i++) {
+    const num = nums[i];
+  }
 }
 
 module.exports = maxProduct;
