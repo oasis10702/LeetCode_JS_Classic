4f749aa826c547cb418cd14f43fbd8cf2d16ef5b
oasis10702 <oasis10702@gmail.com>
2023-02-02 23:25:33 +0900

    Add condition

--- exercises/LC33_search/index.js
+++ exercises/LC33_search/index.js
@@ -1,6 +1,9 @@
 function findMinIdx(rotatedSortedArr) {
   let left = 0;
   let right = rotatedSortedArr.length - 1;
+
+  if (rotatedSortedArr.length === 1) {
+  }
 }
 
 function search(nums, target) {
