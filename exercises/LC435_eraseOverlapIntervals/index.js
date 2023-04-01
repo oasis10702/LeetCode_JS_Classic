5d1a6e01ec7e8549cb4dc847b2f4791a4073f2f8
oasis10702 <oasis10702@gmail.com>
2023-04-01 19:29:40 +0900

    Sort intervals

--- exercises/LC435_eraseOverlapIntervals/index.js
+++ exercises/LC435_eraseOverlapIntervals/index.js
@@ -2,6 +2,8 @@ const eraseOverlapIntervals = (intervals) => {
   if (intervals.length === 0) return 0;
 
   let count = 0;
+
+  intervals.sort((a, b) => a[0] - b[0]);
 };
 
 module.exports = eraseOverlapIntervals;
