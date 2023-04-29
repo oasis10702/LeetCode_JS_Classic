055bccf4ed8eb6b22dec8d70140d0021ca3a3b5f
oasis10702 <oasis10702@gmail.com>
2023-04-29 23:30:59 +0900

    Add loop

--- exercises/LC56_merge/index.js
+++ exercises/LC56_merge/index.js
@@ -4,6 +4,8 @@ const merge = (intervals) => {
   intervals.sort((a, b) => a[0] - b[0]);
 
   const res = [intervals[0]];
+
+  for (let i = 0; i < intervals.length; i++) {}
 };
 
 module.exports = merge;
