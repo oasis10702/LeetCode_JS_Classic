b43f513a005a86e2428dc1b9dd07d0f3e78a81c1
oasis10702 <oasis10702@gmail.com>
2023-08-19 22:28:58 +0900

    Add if

--- exercises/LC73_setZeroes/index.js
+++ exercises/LC73_setZeroes/index.js
@@ -20,7 +20,9 @@ function setZeroes(matrix) {
 
   // Use first row and column as flags, if rest of cells have zeros
   for (let row = 1; row < matrix.length; row++) {
-    for (let col = 1; col < matrix[0].length; col++) {}
+    for (let col = 1; col < matrix[0].length; col++) {
+        if () {}
+    }
   }
 }
 
