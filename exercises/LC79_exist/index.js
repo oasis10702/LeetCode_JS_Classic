e1caae3990a91c9cca0b612af6ca5e313fd07887
oasis10702 <oasis10702@gmail.com>
2023-09-29 23:49:38 +0900

    Add for

--- exercises/LC79_exist/index.js
+++ exercises/LC79_exist/index.js
@@ -1,7 +1,11 @@
 function exist(board, word) {
   let found = false;
 
-  for (let row = 0; row < board.length; row++) {}
+  for (let row = 0; row < board.length; row++) {
+    for (){
+        
+    }
+  }
 }
 
 module.exports = exist;
