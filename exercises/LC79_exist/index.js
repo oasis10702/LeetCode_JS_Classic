6069b02e1fedeb180e4a3c2de11a13f6fa721f66
oasis10702 <oasis10702@gmail.com>
2023-10-02 22:56:57 +0900

    Plus col

--- exercises/LC79_exist/index.js
+++ exercises/LC79_exist/index.js
@@ -2,7 +2,7 @@ function exist(board, word) {
   let found = false;
 
   for (let row = 0; row < board.length; row++) {
-    for (let col = 0){}
+    for (let col = 0; board[0].length; col++) {}
   }
 }
 
