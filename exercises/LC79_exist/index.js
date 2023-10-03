cea765b0715004dd965905d23d89a401f0fae687
oasis10702 <oasis10702@gmail.com>
2023-10-03 22:56:54 +0900

    Add if

--- exercises/LC79_exist/index.js
+++ exercises/LC79_exist/index.js
@@ -2,7 +2,9 @@ function exist(board, word) {
   let found = false;
 
   for (let row = 0; row < board.length; row++) {
-    for (let col = 0; board[0].length; col++) {}
+    for (let col = 0; board[0].length; col++) {
+        if (){}
+    }
   }
 }
 
