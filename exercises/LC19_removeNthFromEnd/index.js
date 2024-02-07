f9a07055b1efc95ee44a8f9dacd271d4d7f4db06
oasis10702 <oasis10702@gmail.com>
2024-02-07 20:42:35 +0900

    .

--- exercises/LC19_removeNthFromEnd/index.js
+++ exercises/LC19_removeNthFromEnd/index.js
@@ -2,6 +2,8 @@ function removeNthFromEnd(head, n) {
   let dummyHead = { next: head };
   let slow = dummyHead;
   let fast = dummyHead;
+
+  // Move fast N nodes ahead of slow.
 }
 
 module.exports = removeNthFromEnd;
