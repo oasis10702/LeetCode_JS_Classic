0e405f622ceea86d910e4f7e5449198d61fae216
oasis10702 <oasis10702@gmail.com>
2024-04-02 19:59:45 +0900

    .

--- exercises/LC21_mergeTwoLists/index.js
+++ exercises/LC21_mergeTwoLists/index.js
@@ -2,7 +2,9 @@ function mergeTwoLists(l1, l2) {
   const dummyHead = { next: null };
   let tail = dummyHead;
 
-  while (l1 && l2) {}
+  while (l1 && l2) {
+    if (){}
+  }
 }
 
 module.exports = mergeTwoLists;
