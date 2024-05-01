156924f9b36cb49475fe28ef2020e704664d734f
oasis10702 <oasis10702@gmail.com>
2024-05-01 23:48:23 +0900

    .

--- exercises/LC200_numIslands/index.js
+++ exercises/LC200_numIslands/index.js
@@ -1,7 +1,9 @@
 function numIslands(grid) {
   let count = 0;
 
-  for (let row = 0; row < grid.length; row++) {}
+  for (let row = 0; row < grid.length; row++) {
+    for() {}
+  }
 }
 
 module.exports = numIslands;
